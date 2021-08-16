fbb5daf7e5621e585016431d005b9b6ee8d1b043
Daniel <danielbped@gmail.com>
2021-08-16 15:52:25 -0300

    Refatora page Preferencia e altera bg-color do dark mode

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Routes from './Routes';
 class App extends Component {
   render() {
     return (
-      <main className="bg-indigo-100 dark:bg-blue-900 transition duration-1000 h-screen">
+      <main className="bg-indigo-100 dark:bg-gray-900 transition duration-1000 h-screen">
         <Header />
         <section className="flex justify-center items-center h-3/4">
           <Provider>
--- src/Pages/Preferencia.jsx
+++ src/Pages/Preferencia.jsx
@@ -1,45 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useContext} from 'react';
 import ButtonNext from '../components/ButtonNext';
 import Radio from '../components/Radio';
 import MyContext from '../context/MyContext';
 
-class Preferencia extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      preference: '',
-    }
-    this.handleChange = this.handleChange.bind(this);
+function Preferencia() {
+  const [ preference, setPreference ] = useState('');
+  const { handleClick } = useContext(MyContext);
+  const handleChange = ({ target: { value }}) => {
+    setPreference(value)
   }
-
-  handleChange({ target: { name, value }}) {
-    this.setState({
-      [name]: value,
-    })
-  }
-
-  render() {
-    const { preference } = this.state;
-    const { handleClick } = this.context;
-    
-    return (
-      <main className="flex flex-col">
-        <Radio
-          label="Escolha o certo:"
-          value={ preference }
-          onChange={ this.handleChange }
-        />
-        <ButtonNext
-          link="/comida"
-          onClick={ () => handleClick('preference', preference) }
-          name={ preference }
-        />
-      </main>
-    )
-  }
+  return (
+  <main className="flex flex-col">
+    <Radio
+      label="Escolha o certo:"
+      value={ preference }
+      onChange={ handleChange }
+    />
+    <ButtonNext
+      link="/comida"
+      onClick={ () => handleClick('preference', preference) }
+      name={ preference }
+    />
+  </main>
+  )
 }
-
-Preferencia.contextType = MyContext;
 
 export default Preferencia;
